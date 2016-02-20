a82d28f57c507ffe0093df5c7d0f5aab6b1d3d35
Raja M <raja1612.m@gmail.com>
2016-02-20 11:11:53 +0530

    Bringing up exodus for oneplusx device Adding exodus parameters

--- exodus.mk
+++ exodus.mk
@@ -19,12 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/onyx/onyx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_onyx
+PRODUCT_NAME := exodus_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := OnePlus X
